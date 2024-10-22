ba7298e2360af737f77a84d4bad449fe41bbf308
lihang <lihang@kilox.cn>
2024-10-22 19:35:44 +0800

    "更新cmake文件"

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# my_slam_auto_driving
+认真学习激光slam的基础，先学习高翔老师的基础知识，再把优化库的一些知识放到这里面
 
+# 安装必要的库
+···
+ sudo apt install -y ros-noetic-pcl-ros ros-noetic-velodyne-msgs libopencv-dev libgoogle-glog-dev libeigen3-dev libsuitesparse-dev libpcl-dev libyaml-cpp-dev libbtbb-dev libgmock-dev
+···
---
+++ cmake/packages.cmake
@@ -0,0 +1,87 @@
+# 包含cmake文件
+list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake)
+
+#eigen 
+find_package(Eigen3 REQUIRED)
+include_directories(${EIGEN3_INCLUDE_DIRS})
+
+#sophus 只依赖头文件
+include_directories(${PROJECT_SOURCE_DIR}/thirdparty/sophus)
+ 
+# glog
+find_package(Glog REQUIRED)
+include_directories(${Glog_INCLUDE_DIRS})
+
+# csparse
+find_package(CSparse REQUIRED)
+include_directories(${CSPARSE_INCLUDE_DIR})
+
+# cholmod
+find_package(Cholmod REQUIRED)
+include_directories(${CHOLMOD_INCLUDE_DIRS})
+
+#spdlog
+find_package(spdlog REQUIRED)
+
+#pcl
+find_package(PCL REQUIRED)
+include_directories(${PCL_INCLUDE_DIRS})
+
+#opencv
+find_package(OpenCV REQUIRED)
+include_directories(${OpenCV_INCLUDE_DIRS})
+
+#yaml-cpp
+find_package(yaml-cpp REQUIRED)
+include_directories(${yaml-cpp_INCLUDE_DIRS})
+
+#g2o
+include_directories(${PROJECT_SOURCE_DIR}/thirdparty/g2o)
+set(g2o_libs
+    ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_stuff.so    
+    ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_core.so    
+    ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_dense.so    
+    ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_solver_csparse.so    
+    ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_cspare_extension.so    
+    ${PROJECT_SOURCE_DIR}/thirdparty/g2o/lib/libg2o_types_sba.so 
+    ${CSPARSE_LIBRARY}
+    ${CHOLMOD_LIBRARY}   
+)
+
+#ceres
+find_package(Ceres REQUIRED)
+
+#gtsam
+find_package(GTSAM REQUIRED QUIET)
+include_directories(${GTSAM_INCLUDE_DIR})
+
+# ros
+set(ROS_PACKAGES
+    roscpp 
+    rospy
+    std_msgs
+    sensor_msgs
+    pcl_ros
+    pcl_conversions
+    geometry_msgs
+    nav_msgs
+    tf
+)
+find_package(catkin QUIET COMPONENTS
+    ${ROS_PACKAGES}
+) 
+
+set(third_party_libs
+    ${catkin_LIBRARIES}
+    ${PCL_LIBRARIES}
+    ${OpenCV_LIBRARIES}
+    ${g2o_libs}
+    ${CERES_LIBRARIES}
+    ${Pangolin_LIBRARIES}
+    ${yaml-cpp_LIBRARIES}
+    gtsam
+    glog
+    gflags
+    tbb
+    spdlog::spdlog_header_only
+)
