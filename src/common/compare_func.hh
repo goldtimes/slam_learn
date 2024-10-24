a5446be474da446cfc674db82a87f97f32b38769
lihang <lihang@kilox.cn>
2024-10-24 14:01:54 +0800

    "模板类的比较和hash函数"

---
+++ src/common/compare_func.hh
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "common/eigen_types.hh"
+
+namespace slam_learn {
+// 声明了一个仿函数
+// N的类型的int
+template <int N>
+struct less_vec {
+    inline bool operator()(const Eigen::Matrix<int, N, 1>& v1, const Eigen::Matrix<int, N, 1>& v2) const;
+};
+
+template <int N>
+struct hash_vec {
+    inline size_t operator()(const Eigen::Matrix<int, N, 1>& v) const;
+};
+
+// 实现一个二维的比较
+template <>
+inline bool less_vec<2>::operator()(const Eigen::Matrix<int, 2, 1>& v1, const Eigen::Matrix<int, 2, 1>& v2) const {
+    return v1[0] < v2[0] || (v1[0] == v2[0] && v1[1] < v2[1]);
+}
+
+// 实现一个三维的比较
+template <>
+inline bool less_vec<3>::operator()(const Eigen::Matrix<int, 3, 1>& v1, const Eigen::Matrix<int, 3, 1>& v2) const {
+    return v1[0] < v2[0] || (v1[0] == v2[0] && v1[1] < v2[1]) || (v1[0] == v2[0] && v1[1] == v2[1] && v1[3] < v2[3]);
+}
+
+// 二维的hash
+template <>
+inline size_t hash_vec<2>::operator()(const Eigen::Matrix<int, 2, 1>& v) const {
+    return size_t(((v[0] * 73856093) ^ (v[1] * 471943)) % 10000000);
+}
+
+// 三维的hash
+template <>
+inline size_t hash_vec<3>::operator()(const Eigen::Matrix<int, 3, 1>& v) const {
+    return size_t(((v[0] * 73856093) ^ (v[1] * 471943) ^ (v[2] * 83492791)) % 10000000);
+}
+}  // namespace slam_learn
