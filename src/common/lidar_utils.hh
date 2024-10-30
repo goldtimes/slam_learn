e5172c18571c2c32e6ff5dd4fa9359a7597602ef
lihang <1019825699@qq.com>
2024-10-30 16:35:17 +0000

    "更新bfnn"

--- src/common/lidar_utils.hh
+++ src/common/lidar_utils.hh
@@ -1,3 +1,13 @@
 #pragma once
 
-namespace lidar_utils {}
+#include "common/sensors/point_type.hh"
+
+namespace slam_learn::lidar_utils {
+// 点云到Eigen的常用的转换函数
+inline Vec3f ToVec3f(const PointXYZI& pt) {
+    return pt.getVector3fMap();
+}
+inline Vec3d ToVec3d(const PointXYZI& pt) {
+    return pt.getVector3fMap().cast<double>();
+}
+}  // namespace slam_learn::lidar_utils
--- src/lidar_slam/ch5/bfnn.cc
+++ src/lidar_slam/ch5/bfnn.cc
@@ -1,6 +1,7 @@
 #include "lidar_slam/ch5/bfnn.hh"
 
 #include <execution>
+#include "common/lidar_utils.hh"
 
 namespace slam_learn::bfnn {
 // 遍历所有的点，找到离point最近的点
@@ -35,5 +36,41 @@ std::vector<int> bfnn_point_k(CloudPtr cloud, const Vec3f& point, int k) {
     std::transform(index_and_dist.begin(), index_and_dist.begin() + k, std::back_inserter(ret),
                    [](const auto& d1) { return d1.index_; });
     return ret;
+}
+// 单线程版本的暴力匹配
+void bfnn_cloud(CloudPtr target_cloud, CloudPtr source_cloud, std::vector<std::pair<size_t, size_t>>& matches) {
+    std::vector<size_t> index(source_cloud->size());
+    // 递增
+    std::for_each(index.begin(), index.end(), [idx = 0](size_t& i) mutable { i = idx++; });
+    matches.resize(index.size());
+    std::for_each(std::execution::seq, index.begin(), index.end(), [&](auto& idx) {
+        matches[idx].second = idx;
+        matches[idx].first = bfnn_point(target_cloud, lidar_utils::ToVec3f(source_cloud->points[idx]));
+    });
+}
+void bfnn_cloud_mt(CloudPtr target_cloud, CloudPtr source_cloud, std::vector<std::pair<size_t, size_t>>& matches) {
+    std::vector<size_t> index(source_cloud->size());
+    // 递增
+    std::for_each(index.begin(), index.end(), [idx = 0](size_t& i) mutable { i = idx++; });
+    matches.resize(index.size());
+    std::for_each(std::execution::par_unseq, index.begin(), index.end(), [&](auto& idx) {
+        matches[idx].second = idx;
+        matches[idx].first = bfnn_point(target_cloud, lidar_utils::ToVec3f(source_cloud->points[idx]));
+    });
+}
+void bfnn_cloud_mt_k(CloudPtr target_cloud, CloudPtr source_cloud, std::vector<std::pair<size_t, size_t>>& matches,
+                     int k) {
+    std::vector<size_t> index(source_cloud->size());
+    // 递增
+    std::for_each(index.begin(), index.end(), [idx = 0](size_t& i) mutable { i = idx++; });
+    // source_cloud中的每个点都找到k个邻近
+    matches.resize(index.size() * k);
+    std::for_each(std::execution::par_unseq, index.begin(), index.end(), [&](auto& idx) {
+        auto k_matches = bfnn_point_k(target_cloud, lidar_utils::ToVec3f(source_cloud->points[idx]), k);
+        for (int i = 0; i < k_matches; ++i) {
+            matches[idx * k + i].second = idx;
+            matches[idx * k + i].first = k_matches[i];
+        }
+    });
 }
 }  // namespace slam_learn::bfnn
