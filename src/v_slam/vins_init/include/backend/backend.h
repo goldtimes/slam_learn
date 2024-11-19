eece2518c7acbc7aa6350bdd62d84d9348c22d2f
lihang <lihang@kilox.cn>
2024-11-19 21:08:23 +0800

    初始化的代码,但是缺少数据集

---
+++ src/v_slam/vins_init/include/backend/backend.h
@@ -0,0 +1,19 @@
+//
+// Created by gaoxiang19 on 11/17/18.
+//
+
+#ifndef SLAM_COURSE_BACKEND_H
+#define SLAM_COURSE_BACKEND_H
+
+#include "v_slam/vins_init/include/backend/edge.h"
+#include "v_slam/vins_init/include/backend/loss_function.h"
+#include "v_slam/vins_init/include/backend/problem.h"
+#include "v_slam/vins_init/include/backend/vertex.h"
+
+#include "v_slam/vins_init/include/backend/vertex_inverse_depth.h"
+#include "v_slam/vins_init/include/backend/vertex_point_xyz.h"
+#include "v_slam/vins_init/include/backend/vertex_pose.h"
+
+#include "v_slam/vins_init/include/backend/edge_reprojection.h"
+
+#endif  // SLAM_COURSE_BACKEND_H
